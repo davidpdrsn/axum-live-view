252d75392e5e703a0d23af84b576504e8007b8c9
David Pedersen <david.pdrsn@gmail.com>
2022-01-02 18:32:39 +0100

    convert examples to JS

--- examples/counter/webpack.config.js
+++ examples/counter/webpack.config.js
@@ -1,20 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: './src/index.js',
   mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
